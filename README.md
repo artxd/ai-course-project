15fd4372d31e9c2d1c29f7e327170d2c3dee9893
Jaagup Kuhi <jaagupku@gmail.com>
2018-01-07 05:49:41 +0200

    update readme and added user or computer before printed sentence

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AI Course Project
+
 Chatbot + speech synthesis.
+
+## Requirements
+
+### Dependencies
+`pip install google-cloud-speech`
+`pip install pyaudio`
+`pip install pynput`
+
+### Google Cloud
+Required Google cloud account with google speech api enabled
+
+Create new `service account key` at google cloud->Api & Services -> Credentials -> As JSON
+Set python environment variable `GOOGLE_APPLICATION_CREDENTIALS=<downloaded json file name>.json`
--- ai.py
+++ ai.py
@@ -2,36 +2,50 @@ import recorder
 import re
 from pynput.keyboard import Key, Listener
 
+
+def printUser(text):
+    print("User:", text)
+
+
+def printComputer(text):
+    print("Computer:", text)
+
+
 def response(text):
-    if("smokes" in text):
+    if "smokes" in text.lower():
         return "Fuck you Ricky!"
 
-print("Hello, my name is Bot!")
+
+printComputer("Hello, my name is Bot!")
 
 while True:
     with recorder.AudioRecorder() as rec:
 
         def on_press(key):
             if key == Key.ctrl_l:
-                if(rec.closed):
+                if rec.closed:
                     rec.start()
-        
+
+
         def on_release(key):
             if key == Key.ctrl_l:
                 rec.stop()
                 return False
-            
+
+
         with Listener(on_press=on_press, on_release=on_release) as listener:
             listener.join()
-            
+
         text = rec.transcribe()
 
-        if text: print(text+"!")
-        else: print(text)
-        
-        if(text.endswith("let's go")):
-            print(response(re.sub("let's go","",text)))
-        elif not text:
-            print("You didn't say anything")
+        if text:
+            printUser(text + "!")
         else:
-            print("Please end your sentence with a command: 'let's go'")
+            printUser(text)
+
+        if text.endswith("let's go"):
+            printComputer(response(re.sub("let's go", "", text)))
+        elif not text:
+            printComputer("You didn't say anything")
+        else:
+            printComputer("Please end your sentence with a command: 'let's go'")
